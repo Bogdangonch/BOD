16adf5fb604e447c46870535f5e673474ecab7f4
Bogdangonch <bogdan.goncharenko95@gmail.com>
2023-10-27 19:31:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  // "Den", "Jack", "Bill", "Antonio"
+  const [posts, setPosts] = useState([
+    {
+      title: "Fruits",
+      textArea: "Banana,Apple,Blueberry",
+    },
+    {
+      title: "Actors",
+      textArea: "Jony,Arnold,Jeyson",
+    },
+    {
+      title: "Street food",
+      textArea: "Hot dogs,pies,pizza",
+    },
+    {
+      title: "Post about how to make money",
+      textArea: "You can never make a lot of money",
+    },
+  ]);
+  const [input, setInput] = useState("");
+  const [textArea, setTextArea] = useState("");
+
+  function handleAddName(event, title) {
+    event.preventDefault();
+
+    if (input === "" || textArea === "") {
+      return;
+    }
+
+    setPosts((prev) => [...prev, { title, textArea: textArea }]);
+    setInput("");
+    setTextArea("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <ul>
+          {posts.map((el, ind) => (
+            <div key={ind}>
+              <h2>{el.title}</h2>
+              <p>{el.textArea}</p>
+            </div>
+          ))}
+        </ul>
+      </main>
+
+      <aside>
+        <h3 style={{ textAlign: "center" }}>Enter your posts</h3>
+        <form>
+          <input
+            type="text"
+            placeholder="Post title"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="Text post"
+            value={textArea}
+            onChange={(e) => setTextArea(e.target.value)}
+          ></textarea>
+          <button
+            className="btn"
+            onClick={(e) => handleAddName(e, input)}
+            disabled={input === "" || textArea === ""}
+          >
+            Add post
+          </button>
+        </form>
+      </aside>
     </div>
   );
 }
